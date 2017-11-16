30d8df82bfad89ab53f8c0a1df8b04e98731837b
Supakorn Trakulmaiphol <wdrdres3qew5ts21@gmail.com>
2017-11-16 20:43:33 +0700

    ทำ Pagination กับ Detail จำ stage ได้แล้ว

--- src/login/blockDisplay/FXMLDetailPageController.java
+++ src/login/blockDisplay/FXMLDetailPageController.java
@@ -20,12 +20,17 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.Pagination;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import javafx.util.Callback;
 import login.FXMLLoginController;
+import static login.FXMLLoginController.pageCallBack;
 
 /**
  * FXML Controller class
@@ -34,6 +39,7 @@ import login.FXMLLoginController;
  */
 public class FXMLDetailPageController extends FXMLLoginController implements Initializable {
 
+    Pagination page = null;
     //int pageCallBack=0;
     @FXML
     private GridPane detailPage;
@@ -57,21 +63,25 @@ public class FXMLDetailPageController extends FXMLLoginController implements Ini
         //ดึงข้อมูลจากระบบมาเขียน
         System.out.println("SQL Room " + sqlRoomId);
         con = MyConnection.getConnection();
-        String sql = "SELECT r.roomId,r.price,t.nType,a.city,r.bedrooms,r.bathrooms,r.sqMeters,\n"
-                + "c.parking,roomId,r.detail FROM room r\n"
+        String sql = "SELECT  distinct r.roomId,r.price,t.nType,a.city,r.bedrooms,r.bathrooms,r.sqMeters,\n"
+                + "c.parking,c.fitness,c.swimming,r.detail FROM room r\n"
                 + "left JOIN condo c ON\n"
                 + "r.condoId = c.condoId\n"
                 + "JOIN area a ON\n"
                 + "c.areaId = a.areaId\n"
                 + "JOIN roomType t ON\n"
                 + "r.typeId = t.typeId\n"
-                + "WHERE roomId=?";
+                + "join metro m ON\n"
+                + "a.areaId = m.areaId\n"
+                + "WHERE r.roomId=?";
         try {
             PreparedStatement pstm = con.prepareStatement(sql);
             pstm.setString(1, sqlRoomId);
-            ResultSet rs=pstm.executeQuery();
+            ResultSet rs = pstm.executeQuery();
             rs.next();
-            
+            System.out.println(detailPage.getChildren().get(0));//รูปคอนโด 
+            ((Label)((AnchorPane)(detailPage.getChildren().get(3))).getChildren().get(0)).setText("SALE/THB"+rs.getDouble("r.price"));//ราคาของคอนโด
+            ((Text)detailPage.getChildren().get(4)).setText(rs.getString("r.detail"));//เขียนข้อมูลคอนโด
         } catch (SQLException ex) {
             Logger.getLogger(FXMLDetailPageController.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -102,17 +112,31 @@ public class FXMLDetailPageController extends FXMLLoginController implements Ini
     @FXML
     private void backButtonOnClick(ActionEvent event) {
         //ใช้ state ก่อนหน้าเพื่อย้อนกลับไป
-        FXMLLoader fxmlPage = new FXMLLoader();
-        fxmlPage.setLocation(FXMLDetailPageController.class.getResource("FXMLPagination.fxml"));
-        FXMLPaginationController pageController = new FXMLPaginationController();
-        fxmlPage.setController(pageController);
-        Pagination page = null;
-        try {
-            page = fxmlPage.load();
-        } catch (IOException ex) {
-            Logger.getLogger(FXMLDetailPageController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        mainView.setCenter(page);
+//        FXMLLoader fxmlPage = new FXMLLoader();
+//        fxmlPage.setLocation(FXMLDetailPageController.class.getResource("FXMLPagination.fxml"));
+//        FXMLPaginationController pageController = new FXMLPaginationController();
+//
+//        fxmlPage.setController(pageController);
+        System.out.println("Page Call Back : " + pageCallBack);
+
+        page = legacyPage;
+//        page.setPageFactory(new Callback<Integer, Node>() {
+//            //ทำการเขียน Anoonymous Class ข้างในไปเลยเพื่อจะใช้ method สร้างหน้า
+//            //method Call มีการ new Object ทุกครั้งที่สร้างหน้าใหม่ซึ่งดูแล้วเปลือง memory จริงๆควรจะทำ ArrayList
+//            //เอาไว้ดักเวลากดหน้าเช่นกดหน้า 1 ก็จำเม็มข้อมูลหน้า 1 ไว้แล้ววันหลังก็เอาไปใช้ได้เลย แต่ขอพักไว้ก่อน ไว้ทำตอนว่างล่ะกันนะจ้ะ
+//            @Override
+//            public Node call(Integer pageIndex) {
+//                pageIndex = pageCallBack;
+//
+//                pageCallBack = pageIndex;//บันทึก state ไว้ว่ากดมาจากหน้าไหน
+//                System.out.println("New Object !!!");
+//                System.out.println("หน้าที่จะไป : " + pageIndex);
+//                return this.page.get(pageIndex);//createPage(pageIndex);
+//
+//            }
+//        }
+//        );
+        legacyMainView.setCenter(page);
         System.out.println("กดปุ่ม !!!");
     }
 
